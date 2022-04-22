4eaa89f6dcda402419a8a3978a88aac2ca524b49
Zehra  Erdem <zehra.erdem@ug.bilkent.edu.tr>
2022-04-22 16:07:56 +0300

    db is connected

--- group14/src/main/java/cs353/group14/Group14Application.java
+++ group14/src/main/java/cs353/group14/Group14Application.java
@@ -2,12 +2,47 @@ package cs353.group14;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class Group14Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Group14Application.class, args);
+
+		/*
+		try {
+
+			Connection connection =  DriverManager.getConnection("jdbc:postgresql://group14.ckx3kijsemgc.us-east-2.rds.amazonaws.com:5432/", "postgres", "postgrelc");
+
+			Statement statement = connection.createStatement();
+
+			// drop table
+			String s = "CREATE TABLE users( " +
+					"user_id SERIAL PRIMARY KEY, " +
+					"username VARCHAR(31) NOT NULL UNIQUE, " +
+					"password VARCHAR(31) NOT NULL, " +
+					"mail VARCHAR(255) NOT NULL UNIQUE, " +
+					"name VARCHAR(31) NOT NULL, " +
+					"profile_photo BYTEA, " +
+					"information VARCHAR(1023)) ";
+
+			statement.executeUpdate(s);
+			System.out.println("there");
+
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+
+		 */
+
+
 	}
 
 }

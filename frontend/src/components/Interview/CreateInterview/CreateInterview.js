8d9b04e20cd06b01fe6577f0e4f045d586eefab1
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-05-01 15:51:51 +0300

    Create new interview logic part 1 added

--- frontend/src/components/Interview/CreateInterview/CreateInterview.js
+++ frontend/src/components/Interview/CreateInterview/CreateInterview.js
@@ -5,9 +5,10 @@ import Autocomplete from '@mui/material/Autocomplete';
 import Stack from '@mui/material/Stack';
 import { Box, FormControl, TextField, MenuItem, Typography, Button, Select} from "@material-ui/core";
 import Grid from '@mui/material/Grid';
+import {CreateNewInterview} from './CreateInterviewAPI'
 
 
-const timeUnit = [
+const timeUnits = [
   {
     value: 'min'
   },
@@ -24,11 +25,33 @@ function CreateInterview(props) {
   const [position, setPosition] = React.useState('');
   const [timeUnit, setTimeUnit] = useState('min');
   const [duration, setDuration] = useState(timeUnit[0].value);
-  //const createNonCodingQuestionAPI = new CreateNonCodingQuestionAPI();
+  const createNewInterview = new CreateNewInterview();
+  let durationInMin = 0;
 
   async function handleSubmit() {
 
+    console.log(timeUnit)
+    if(timeUnit === 'min')
+    {
+        durationInMin = duration;
+    }
+    else if (timeUnit === 'hour(s)')
+    {
+        durationInMin = duration * 60;
+    }
+    else if( timeUnit === 'day(s)')
+    {
+        durationInMin = duration * 60*24;
+    }
 
+    console.log(durationInMin);
+    const newInterview = {
+        user_id: localStorage.getItem('userId'),
+        interview_id: -1,
+        duration: durationInMin,
+        position: position
+      }
+      await createNewInterview.createInterview(newInterview);
 
 }
 
@@ -91,23 +114,27 @@ function CreateInterview(props) {
                 </div>
               </Grid>
               <Grid item xs={6}>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={timeUnit}
-                    label="Unit"
-                    onChange={(e) => handleSubmit(e)}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
+              <div>
+                  <TextField
+                      id="outlined-select-currency"
+                      select
+                      margin="normal"
+                      value={timeUnit}
+                      onChange={(e) => setTimeUnit(e.target.value)}
+                    >
+                      {timeUnits.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.value}
+                        </MenuItem>
+                      ))}
+                      </TextField>
+                  </div>
               </Grid>
               </Grid>
               <div>
                </div>
               </Box>
-              <Grid container spacing={12} style= {{marginTop: "1rem"}} >
+              <Grid container spacing={12} >
               <Grid item  justify="flex-end">
                   <Button
                   type="submit"
@@ -128,6 +155,68 @@ function CreateInterview(props) {
             </Grid>
             </FormControl>
           </Grid>
+          <div >
+              <Grid container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 8 }}>
+                     <Grid item xs={6}>  
+                    <Grid
+                            container
+                            direction="row"
+                            justifyContent="center"
+                            alignItems="center"
+                
+                        >
+                            <Grid item>  
+                                <Typography variant="h6" > Position</Typography>
+                            </Grid>  
+                    </Grid> 
+                  </Grid>
+                 <Grid item xs={6}>  
+                    <Grid
+                            container
+                            direction="row"
+                            justifyContent="center"
+                            alignItems="center"
+                
+                        >
+                            <Grid item >  
+                                <Typography variant="h6" > Duration</Typography>
+                            </Grid>  
+                    </Grid> 
+                  </Grid>          
+              </Grid>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ mt: 8 }}
+            >
+                <Grid item xs={6} >
+                    <Grid container  direction="row" justifyContent="center" alignItems="center">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSubmit}
+                        > Add Coding Question
+                    </Button>
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                <Grid container  direction="row" justifyContent="center" alignItems="center">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSubmit}
+                        > Add Coding Question
+                    </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
         </div>
     );
     
---
+++ frontend/src/components/Interview/CreateInterview/CreateInterviewAPI.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+//import {MessageResponse, MessageType} from "../common/dto/MessageResponse";
+
+export interface newInterview {
+    user_id: number,
+    interview_id: number,
+    duration: number,
+    position: string
+       
+}
+
+
+export class CreateNewInterview {
+
+    async createInterview(newQuestion: newInterview) {
+        const response = await axios.put("/createInterview/", newQuestion);
+        return response.data;
+    }
+
+    
+}

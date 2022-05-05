2d9781a0bb480cd8a31625d3af58869006e58255
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-05-05 18:58:54 +0300

    UI added for start inteview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import CreateNonCodingQuestion from './components/NonCodingChallenges/CreateNonC
 import CreateInterview from './components/Interview/CreateInterview/CreateInterview';
 import CodingChallengeTable from './components/Contest/CreateContest/CodingChallengeTable';
 import AddParticipants from './components/Interview/CreateInterview/AddParticipants';
+import StartInterview from './components/Interview/AttendInterview/StartInterview';
 function App() {
   return (
     <BrowserRouter>
@@ -41,6 +42,8 @@ function App() {
       <Route path = {"/CreateInterview"} element = {<CreateInterview/>}/>
       <Route path = {"/AddContestChallenges"} element = {<CodingChallengeTable/>}/>
       <Route path = {"/AddParticipants"} element = {<AddParticipants/>}/>
+      <Route path = {"/StartInterview"} element = {<StartInterview/>}/>
+
 
 
 
